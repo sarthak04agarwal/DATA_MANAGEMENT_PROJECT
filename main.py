d2b0dec8a1b07dd61edb64148bca81bb285b5add
sarthak04agarwal <sarthak04agarwal@gmail.com>
2022-12-15 12:37:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import json
 
-# Open a file and use json.load to load JSON data from file as data
-with open("song-data.json", "r") as file_ref:
-    song_Data = json.load(file_ref)
+file = open("song-data.json", "r")
+dataStr = file.read()
+file.close()
 
-
+song_Data = json.loads(dataStr)
+print(song_Data)
 
 
 def display_All():
     for i in range (len(song_Data)):
+        print(song_Data)
         print("Title " + song_Data[i]['title'])
         print("Artist " + song_Data[i]['artist'])
         print("Genre " + song_Data[i]['genre'])
         print("length " + song_Data[i]['length'])
         print("language " + song_Data[i]['language'])
-        print("released " + song_Data[i]['released'] + "\n")
-        
+        print("released " + song_Data[i]['released'])
+
+print(display_All)
 
 menu = ("1. Display All Data \n2. Display some of the data \n3. Sort the data \n4. Add data to favorite list \n5. Remove data from favorite \n6. Display Favorite List \n7. Exit" + "\n Please type your number: ")
 
@@ -25,7 +28,7 @@ loop = True
 while(loop):
     selection = input(menu)
     if selection == "1":
-        print(display_All)
+        display_All()
     elif selection == "2":
         print("Hello")
     elif selection == "3":
@@ -38,5 +41,3 @@ while(loop):
         print("Hello")
     else:
         loop = False
-
-
