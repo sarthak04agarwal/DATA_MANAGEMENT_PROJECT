f6580a6c9c19233d1b6e756ad6683f15b5151495
sarthak04agarwal <sarthak04agarwal@gmail.com>
2022-12-16 13:05:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,13 @@ print(song_Data)
 
 def display_All():
     for i in range (len(song_Data)):
-        print(song_Data)
-        print("Title " + song_Data[i]['title'])
-        print("Artist " + song_Data[i]['artist'])
-        print("Genre " + song_Data[i]['genre'])
-        print("length " + song_Data[i]['length'])
-        print("language " + song_Data[i]['language'])
-        print("released " + song_Data[i]['released'])
+        print("Title: " + song_Data[i]['title'])
+        print("Artist: " + song_Data[i]['artist'])
+        print("Genre: " + song_Data[i]['genre'])
+        print("Length: " + str(song_Data[i]['length']))
+        print("Language: " + song_Data[i]['language'])
+        print("Released: " + song_Data[i]['released'] + '\n')
 
-print(display_All)
 
 menu = ("1. Display All Data \n2. Display some of the data \n3. Sort the data \n4. Add data to favorite list \n5. Remove data from favorite \n6. Display Favorite List \n7. Exit" + "\n Please type your number: ")
 
