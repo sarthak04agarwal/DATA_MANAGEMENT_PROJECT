c0e811cb3acf0d2135f46e1345730938a4f0d269
sarthak04agarwal <sarthak04agarwal@gmail.com>
2023-01-09 18:31:57 -0700

    y

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import json
 
-
+# Loads the song-data JSON file
 with open("song-data.json", "r") as file_ref:
     song_Data = json.load(file_ref)
 
+# Loads the user-data JSON file
 with open("user-data.json", "r") as file_ref:
     user_Data = json.load(file_ref)
 
-
+# Method that displays all the song data
 def display_All():
     for i in range (len(song_Data)):
         print("Title: " + song_Data[i]['title'])
@@ -17,12 +18,35 @@ def display_All():
         print("Language: " + song_Data[i]['language'])
         print("Released: " + song_Data[i]['released'] + '\n')
 
+def add_Favorite():
+    if loggedIn:
+        for i in range(len(song_Data)):
+            print(song_Data[i]['title'] + ': ' + str(i+1))
+        add_Fav = input("Please select the number which correlaates to the song that you want to add to you favorite list: ")
+        add_Fav = int(add_Fav)-1
+        if song_Data[add_Fav]['title'] in user_Data[loggedInPos]['faves']:
+            print(song_Data[add_Fav]['title'] + " is already in your favorite list.")
+        else:
+            user_Data[loggedInPos]['faves'].append(song_Data[int(add_Fav)]['title'])
+            print(song_Data[add_Fav]['title'] + " has been added to your favorite list.")
 
-#def display_Favorite():
-   
+def remove_Favorite():
+    if loggedIn:
+        for i in range(len(user_Data[loggedInPos]['faves'])):
+            print(user_Data[loggedInPos]['faves'][i] + ': ' + str(i+1))
+        rem_Fav = input("Please select the number which correlaates to the song that you want to remove from your favorite list: ")
+        if user_Data[loggedInPos]['faves'][int(rem_Fav)-1] in user_Data[loggedInPos]['faves']:
+            print(user_Data[loggedInPos]['faves'])
+            removed = user_Data[loggedInPos]['faves'].pop(int(rem_Fav)-1)
+            print(removed + " has been removed from your favorite list.")
+            print(user_Data[loggedInPos]['faves'])
+        else:
+            print("Please enter one of the numbers above.")
 
-	
-#menu = ("1. Display All Data \n2. Display some of the data \n3. Sort the data \n4. Add data to favorite list \n5. Remove data from favorite \n6. Display Favorite List \n7. Exit" + "\n Please type your number: ")
+
+def display_Favorite():
+    print(user_Data[loggedInPos]['username'] + "'s Favorites: " + user_Data[loggedInPos]['faves'])
+       
 
 
 loop = True
@@ -33,12 +57,35 @@ def displayMenu():
     status = input("\n1. Log in to account\n2. Create new account\n3. Log out of account\n Please type your number: ")
     if status == "1":
         oldUser()
+        if(loggedIn):
+            displaySecMenu()
     elif status == "2":
         newUser()
     elif status == '3':
         logOut()
     else:
         loop = False
+
+# Method that displays the second menu 
+def displaySecMenu():
+    loop2 = True
+    while(loop2):
+        selection = input("1. Display All Data \n2. Display some of the data \n3. Sort the data \n4. Add data to favorite list \n5. Remove data from favorite \n6. Display Favorite List \n7. Exit" + "\n Please type your number: ")
+        if selection == "1":
+            display_All()
+        elif selection == "2":
+            print("Hello")
+        elif selection == "3":
+            print("Hello")
+        elif selection == "4":
+            print("Hello")
+        elif selection == "5":
+            print("Hello")
+        elif selection == "6":
+            display_Favorite()
+        else:
+            loop2 = False
+
 
 # Method to create a new account
 def newUser():
@@ -63,16 +110,18 @@ def newUser():
 
 # Method for signing into an account            
 def oldUser():
-    global loggedIn
     login = input("Enter login name: ")
     usernameFound = False
     for i in range(len(user_Data)):
             if user_Data[i]['username'] == login:
                 passw = input("Enter password: ")
                 if user_Data[i]['password'] == passw:
-                    print("User found. Logged in")
+                    global loggedInPos
+                    global loggedIn
                     loggedIn = True
+                    loggedInPos = i
                     usernameFound = True
+                    print("User found. Logged in")
                 else:
                     print("Password incorrect")
                     usernameFound = True
@@ -84,31 +133,13 @@ def logOut():
     if loggedIn == True:
         global loggedIn
         loggedIn = False
+        global loggedInPos
+        loggedInPos = None
         print("Logged out successfully.")
 
 
 
 while loop:
     displayMenu()
-
-
-'''
-loop = True
-while(loop):
-    selection = input(menu2)
-    if selection == "1":
-        display_All()
-    elif selection == "2":
-        print("Hello")
-    elif selection == "3":
-        print("Hello")
-    elif selection == "4":
-        print("Hello")
-    elif selection == "5":
-        print("Hello")
-    elif selection == "6":
-        print("Hello ")
-    else:
-        loop = False
-'''
-
+with open("user-data.json", "w") as file_ref:
+    json.dump(user_Data, file_ref)
